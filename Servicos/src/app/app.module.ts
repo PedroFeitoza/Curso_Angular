203a8676210f024baca315e18acc755906bb6e5b
Pedro Henrique <pedrooheenrique159@gmail.com>
2021-08-04 17:56:39 -0300

    finalizando serviços

---
+++ Servicos/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CursosModule,
+    CriarCursoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
